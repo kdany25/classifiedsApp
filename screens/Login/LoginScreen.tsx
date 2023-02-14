dfbffe4b1c8fd17b0b04f510c6d2048d878c6888
kdany25 <kabadany25@gmail.com>
2023-02-14 21:30:55 +0200

    feat: added login form

---
+++ screens/Login/LoginScreen.tsx
@@ -0,0 +1,63 @@
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+	View,
+	Text,
+	ImageBackground,
+	SafeAreaView,
+	TextInput,
+	Pressable,
+} from "react-native";
+import { styles } from "../LoginScreen.style";
+import { SocialIcon } from "react-native-elements";
+
+const LoginScreen = () => {
+	const navigation = useNavigation();
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerShown: false,
+		});
+	}, []);
+
+	return (
+		<SafeAreaView style={styles.main}>
+			<View style={styles.container}>
+				<Text style={styles.title}>Get started</Text>
+			</View>
+			<View>
+				<View style={styles.inputHolder}>
+					<View style={styles.labelContainer}>
+						<Text>Email</Text>
+					</View>
+					<View style={styles.inputContainer}>
+						<TextInput placeholder="Enter Email" />
+					</View>
+				</View>
+				<View style={styles.inputHolder}>
+					<View style={styles.labelContainer}>
+						<Text>Password</Text>
+					</View>
+					<View style={styles.inputContainer}>
+						<TextInput placeholder="Enter Password" />
+					</View>
+				</View>
+			</View>
+
+			<View style={{ alignItems: "center" }}>
+				<Pressable style={styles.button}>
+					<Text style={styles.text}>{"Sign in"}</Text>
+				</Pressable>
+			</View>
+			<Text style={styles.signInText}>Sign In with this</Text>
+			<View style={styles.socialMedia}>
+				<SocialIcon type="facebook" />
+				<SocialIcon type="twitter" />
+				<SocialIcon type="google" />
+				<SocialIcon type="instagram" />
+			</View>
+			<Text style={styles.signUpText}>Don't have an account? Signup</Text>
+		</SafeAreaView>
+	);
+};
+
+export default LoginScreen;
---
+++ screens/LoginScreen.style.ts
@@ -0,0 +1,96 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	main: {
+		flex: 1,
+		position: "relative",
+		backgroundColor: "#8d8e8f",
+	},
+	container: {
+		position: "absolute",
+		backgroundColor: "#ffffff",
+		top: "20%",
+		left: "0%",
+		width: "100%",
+		height: "100%",
+		borderTopLeftRadius: 50,
+		borderTopRightRadius: 50,
+	},
+	image: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center",
+	},
+	title: {
+		color: "#416FDF",
+		textAlign: "center",
+		marginTop: "5%",
+		marginBottom: "5%",
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+	labelContainer: {
+		backgroundColor: "white",
+		paddingHorizontal: 3,
+		marginStart: 10,
+		zIndex: 1,
+		elevation: 1,
+		shadowColor: "white",
+		position: "absolute",
+		top: "-20%",
+		left: "5%",
+	},
+	inputContainer: {
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		zIndex: 0,
+		width: "90%",
+		borderColor: "#d5d7db",
+	},
+	inputHolder: {
+		alignItems: "center",
+		marginBottom: "5%",
+	},
+
+	checkboxContainer: {
+		flexDirection: "row",
+		marginBottom: 20, // to be removed
+	},
+	checkbox: {
+		alignSelf: "center",
+	},
+	label: {
+		margin: 8,
+	},
+	button: {
+		alignItems: "center",
+		justifyContent: "center",
+		paddingVertical: 15,
+		borderRadius: 10,
+		elevation: 3,
+		backgroundColor: "#416FDF",
+		width: "90%",
+	},
+	text: {
+		fontSize: 16,
+		lineHeight: 21,
+		fontWeight: "bold",
+		letterSpacing: 0.25,
+		color: "white",
+	},
+	signInText: {
+		color: "#c2b9b8",
+		textAlign: "center",
+		marginTop: 20, // to be removed
+	},
+	socialMedia: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		margin: 50, // to be removed
+	},
+	signUpText: {
+		color: "#c2b9b8",
+		textAlign: "center",
+	},
+});
