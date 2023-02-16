942e9b233a96d5960006cb974885c1d6d8088641
kdany25 <kabadany25@gmail.com>
2023-02-16 17:38:23 +0200

    feat: updated create product page UI

--- screens/create-Product/createProduct.styles.ts
+++ screens/create-Product/createProduct.styles.ts
@@ -2,19 +2,47 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create<any>({
 	input: {
-		borderColor: "gray",
 		width: "100%",
-		borderWidth: 1,
-		borderRadius: 10,
+		borderRadius: 15,
 		padding: 10,
+		marginTop: "10%",
+		backgroundColor: "#f7f4f2",
 	},
-  button: {
+	button: {
 		alignItems: "center",
 		justifyContent: "center",
 		paddingVertical: 15,
 		borderRadius: 10,
 		elevation: 3,
-		backgroundColor: "#416FDF",
-		width: "90%",
+		backgroundColor: "#ff833c",
+	},
+	container: {
+		backgroundColor: "white",
+		marginTop: "10%",
+		width: "100%",
+	},
+	dropdown: {
+		borderRadius: 15,
+
+		backgroundColor: "#f7f4f2",
+
+		paddingHorizontal: 30,
+	},
+	icon: {
+		marginRight: 5,
+	},
+	placeholderStyle: {
+		fontSize: 16,
+	},
+	selectedTextStyle: {
+		fontSize: 16,
+	},
+	iconStyle: {
+		width: 20,
+		height: 20,
+	},
+	inputSearchStyle: {
+		height: 40,
+		fontSize: 16,
 	},
 });
